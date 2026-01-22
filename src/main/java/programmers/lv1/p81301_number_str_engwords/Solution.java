c670a955d850c8f452847a68e4f5406d0f004964
Yang1-_- <wonjjang44@gmail.com>
2026-01-23 02:51:21 +0900

    숫자 문자열과 영단어

---
+++ src/main/java/programmers/lv1/p81301_number_str_engwords/Solution.java
@@ -0,0 +1,59 @@
+package programmers.lv1.p81301_number_str_engwords;
+
+/**
+ * 은광이형 방식대로
+ */
+public class Solution {
+
+    public int solution(String s) {
+        String[] wordsArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        // 영단어 조립용
+        StringBuilder engWord = new StringBuilder();
+
+        // 최종적으로 리턴할 숫자 문자열
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // 문자열에 숫자가 포함돼 있는지 체크(숫자는 비교할 필요 X)
+            if('0' <= c && c <= '9') {
+                result.append(c);
+
+                continue;
+            }
+
+            engWord.append(c);
+
+            for (String string : wordsArr) {
+                if (engWord.toString().equals(string)) {
+                    result.append(engWord);
+
+                    // 초기화
+                    engWord.setLength(0);
+                    break;
+                }
+            }
+
+        }
+
+        return Integer.parseInt(result.toString());
+
+
+        // replaceAll 메서드를 이용한 방법
+//        for (int i = 0; i < wordsArr.length; i++)
+//            s = s.replaceAll(wordsArr[i], String.valueOf(i));
+//
+//        return Integer.parseInt(s);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution("one4seveneight");
+        System.out.println("result = " + result);
+    }
+}
